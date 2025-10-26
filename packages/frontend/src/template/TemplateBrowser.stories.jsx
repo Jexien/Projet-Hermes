bf121bc2a5a82d1fe2a4411adb1386819b49841a
Jexien <141679423+Jexien@users.noreply.github.com>
2025-10-26 15:18:50 +0100

    chore(frontend): add storybook config and example stories

---
+++ packages/frontend/src/template/TemplateBrowser.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default {
+  title: 'Template/TemplateBrowser'
+}
+
+export const Empty = () => (
+  <div style={{padding:20}}>No templates (placeholder)</div>
+)
